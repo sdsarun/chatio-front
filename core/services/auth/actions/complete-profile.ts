e1c9cec54cecb8dafdabe7bd33abd63d94537c04
Sarun Daunghirun <sdsarun@outlook.com>
2025-04-18 01:23:32 +0700

    feat(auth): add check session before complete profile

--- core/services/auth/actions/complete-profile.ts
+++ core/services/auth/actions/complete-profile.ts
@@ -1,22 +1,49 @@
 "use server"
 
 // core
-import { executeGraphQL } from "@/core/services/graphql/execute";
 import { UpdateUserProfileMutation } from "@/core/services/auth/graphql/mutations";
+import { executeGraphQL } from "@/core/services/graphql/execute";
+import { verifySession } from "@/core/lib/dal";
 
 // constants
-import { UserGenderType } from "@/core/services/graphql/graphql";
+import { type UpdateUserProfileMutation as UpdateUserProfileResult, UserGenderType } from "@/core/services/graphql/graphql";
+import { ServerActionResult } from "@/core/types/server-action";
 
 export type CompleteProfileDTO = {
   userId: string;
   gender: UserGenderType;
 }
 
+export type CompleteProfileResult = ServerActionResult<UpdateUserProfileResult>
+
 export async function completeProfile(
   dto: CompleteProfileDTO
-) {
-  return executeGraphQL(UpdateUserProfileMutation, { 
+): Promise<CompleteProfileResult> {
+  const session = await verifySession();
+
+  const { data, errors } = await executeGraphQL(UpdateUserProfileMutation, {
     userId: dto.userId,
     updateUserProfile: { gender: dto.gender }
+  }, {
+    requestOptions: {
+      headers: {
+        Authorization: `Bearer ${session?.token?.accessToken}`
+      }
+    }
   });
+
+  if (errors) {
+    return {
+      success: false,
+      error: errors.map(({ message, extensions }) => ({ 
+        code: extensions?.code as string,
+        message
+      }))
+    }
+  }
+
+  return {
+    success: true,
+    data: data!,
+  }
 }
