af1928dfccf2cd601330686e180dd56f222a2f79
Sarun Daunghirun <sdsarun@outlook.com>
2025-04-17 20:28:30 +0700

    chore(chat): remove dummy component

--- app/c/new/page.tsx
+++ app/c/new/page.tsx
@@ -1,12 +1,10 @@
 import { Button } from '@/core/components/ui/button';
-import { ModeToggle } from '@/core/components/ui/toggle-theme';
 import { signOut } from '@/core/lib/auth';
 import { LoaderCircle } from 'lucide-react';
 
 export default async function NewChatPage() {
   return (
     <div>
-      <ModeToggle />
       <LoaderCircle className='animate-spin' />
       <Button onClick={async () => {
         "use server"
