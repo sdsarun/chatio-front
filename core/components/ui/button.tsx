5a453e723d3cf7bf3f1000105a53c87ddbf0f88a
Sarun Daunghirun <sdsarun@outlook.com>
2025-04-27 05:34:21 +0700

    fix(ui): add more variant size for button

--- core/components/ui/button.tsx
+++ core/components/ui/button.tsx
@@ -23,7 +23,11 @@ const buttonVariants = cva(
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-        icon: "size-9"
+        icon: "size-9",
+        "icon-xs": "size-5",
+        "icon-sm": "size-[22px]",
+        "icon-md": "size-7",
+        "icon-lg": "size-9",
       }
     },
     defaultVariants: {
